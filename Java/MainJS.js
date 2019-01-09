1627dc808a0f09cbeca361df3d4df5bad278af61
StellaAstraea <mattegg2011@hotmail.co.uk>
2019-01-09 23:22:50 +0000

    first commit

--- Java/MainJS.js
+++ Java/MainJS.js
@@ -1,9 +1,10 @@
+//Javascript loops the images around, having the user click and drag them
 $(document).ready(function(){
 	console.log("ready");
 	$(".owl-carousel").owlCarousel({
 		margin: 10,
 		loop:true,
 		autoWidth:true,
-		items:3
+		items:4
 	});
 });
